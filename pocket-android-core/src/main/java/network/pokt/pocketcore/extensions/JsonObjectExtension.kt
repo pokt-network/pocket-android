6774ce85b1ad418ef4063d2d05401c2ff54ec5b1
eugenio35 <eugenioamancebo@gmail.com>
2019-03-18 19:04:36 -0400

    Adding extension to JsonObject

--- pocket-android-core/src/main/java/network/pokt/pocketcore/extensions/JsonObjectExtension.kt
+++ pocket-android-core/src/main/java/network/pokt/pocketcore/extensions/JsonObjectExtension.kt
@@ -1,2 +1,11 @@
 package network.pokt.pocketcore.extensions
 
+import org.json.JSONObject
+
+fun JSONObject.hasError(): Boolean {
+    return this.has("error")
+}
+
+fun JSONObject.getErrorMessage(): String{
+    return this.getJSONObject("error").getString("message")
+}
