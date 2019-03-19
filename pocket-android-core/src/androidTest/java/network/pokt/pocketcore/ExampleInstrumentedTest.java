9df02b02c5dac09f91f4cfc254edd5412902b88c
eugenio35 <eugenioamancebo@gmail.com>
2019-03-19 17:54:08 -0400

    Adding 1st test

--- pocket-android-core/src/androidTest/java/network/pokt/pocketcore/ExampleInstrumentedTest.java
+++ pocket-android-core/src/androidTest/java/network/pokt/pocketcore/ExampleInstrumentedTest.java
@@ -1,18 +1,11 @@
 package network.pokt.pocketcore;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-
 import kotlin.Unit;
-import network.pokt.pocketcore.model.Blockchain;
-import network.pokt.pocketcore.model.Configuration;
-import network.pokt.pocketcore.model.Node;
-import network.pokt.pocketcore.net.PocketAPI;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -24,21 +17,33 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
 
+    PocketCore pocketCore;
+
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("network.pokt.pocketcore.test", appContext.getPackageName());
-
-        PocketCore pocketCore = new PocketCore("DEVID1", "ETH", "4", "0", 5, 1000);
+    public void retrieveFailedNodes() {
         pocketCore.retrieveNodes(nodes -> {
-            for(Node node : nodes){
-
-            }
-
+            assertNull("expected crash",nodes);
             return Unit.INSTANCE;
         });
 
+    }
+
+    @Test
+    public void retrieveNodes() {
+        pocketCore.retrieveNodes(nodes -> {
+            assertNotNull(nodes);
+            return Unit.INSTANCE;
+        });
+
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        this.pocketCore = new PocketCore("DEVID1", "ETH", "4", "0", 5, 1000);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        this.pocketCore = null;
     }
 }
