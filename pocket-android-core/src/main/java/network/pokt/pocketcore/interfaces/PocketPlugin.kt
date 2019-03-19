e58b877c2b975f6f827426b64a080fae7af4abdb
eugenio35 <eugenioamancebo@gmail.com>
2019-03-18 21:06:41 -0400

    Adding PocketPluggin interface

---
+++ pocket-android-core/src/main/java/network/pokt/pocketcore/interfaces/PocketPlugin.kt
@@ -0,0 +1,8 @@
+package network.pokt.pocketcore.interfaces
+
+import network.pokt.pocketcore.model.Wallet
+
+interface PocketPlugin {
+    fun createWallet(subnetwork: String, data: String): Wallet
+    fun importWallet(address: String, privateKey: String, subnetwork: String, data: String)
+}
---
+++ pocket-android-core/src/main/java/network/pokt/pocketcore/model/Wallet.kt
@@ -0,0 +1,4 @@
+package network.pokt.pocketcore.model
+
+class Wallet {
+}
