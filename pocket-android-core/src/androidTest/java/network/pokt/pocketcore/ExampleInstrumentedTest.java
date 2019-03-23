b5a359ff4a1a3d3095cd1a2c3eb4dafe0b84d2d6
eugenio35 <eugenioamancebo@gmail.com>
2019-03-23 09:43:11 -0400

    Adding send report and send relay test methods

--- pocket-android-core/src/androidTest/java/network/pokt/pocketcore/ExampleInstrumentedTest.java
+++ pocket-android-core/src/androidTest/java/network/pokt/pocketcore/ExampleInstrumentedTest.java
@@ -2,10 +2,14 @@ package network.pokt.pocketcore;
 
 import android.support.test.runner.AndroidJUnit4;
 import kotlin.Unit;
-import org.junit.After;
-import org.junit.Before;
+import network.pokt.pocketcore.exceptions.PocketError;
+import network.pokt.pocketcore.model.Relay;
+import network.pokt.pocketcore.model.Report;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
+import static org.hamcrest.CoreMatchers.containsString;
 
 import static org.junit.Assert.*;
 
@@ -17,35 +21,69 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
 
-    PocketCore pocketCore;
 
-    @Test
-    public void retrieveFailedNodes() {
-        pocketCore.retrieveNodes(nodes -> {
-            if(nodes == null){
-                fail("Nodes are null");
-            }
-            return Unit.INSTANCE;
-        });
-
-    }
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
 
     @Test
     public void retrieveNodes() {
+        PocketCore pocketCore = new PocketCore("DEVID1", "ETH", "4", 5, 1000);
         pocketCore.retrieveNodes(nodes -> {
             assertNotNull(nodes);
+            assertFalse(nodes.isEmpty());
+
             return Unit.INSTANCE;
         });
-
     }
 
-    @Before
-    public void setUp() throws Exception {
-        this.pocketCore = new PocketCore("DEVID5", "ETH", "4", "0", 5, 1000);
+    @Test
+    public void failRetrievingNodes() {
+        PocketCore pocketCoreFail = new PocketCore("DEVID1", "ETH2", "4", 5, 1000);
+        pocketCoreFail.retrieveNodes(nodes -> {
+            exception.expect(PocketError.class);
+            return Unit.INSTANCE;
+        });
     }
 
-    @After
-    public void tearDown() throws Exception {
-        this.pocketCore = null;
+    @Test
+    public void sendRelay() {
+        PocketCore pocketCore = new PocketCore("DEVID1", "ETH", "4", 5, 1000);
+        String address = "0xf892400Dc3C5a5eeBc96070ccd575D6A720F0F9f";
+        String data = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBalance\",\"params\":[".concat(address).concat(",\"latest\"],\"id\":67}");
+        Relay relay = pocketCore.createRelay("ETH", "4", data, "DEVID1");
+
+        assertTrue(relay.isValid());
+
+        pocketCore.retrieveNodes(nodes -> {
+            assertNotNull(nodes);
+            assertFalse(nodes.isEmpty());
+            pocketCore.send(relay, jsonObject -> {
+                assertNotNull(jsonObject);
+                assertFalse(jsonObject.has("jsonrcp"));
+                return Unit.INSTANCE;
+            });
+
+            return Unit.INSTANCE;
+        });
+    }
+
+    @Test
+    public void sendReport() {
+        PocketCore pocketCore = new PocketCore("DEVID1", "ETH", "4", 5, 1000);
+        pocketCore.retrieveNodes(nodes -> {
+            assertNotNull(nodes);
+            assertFalse(nodes.isEmpty());
+            Report report = pocketCore.createReport(nodes.get(0).getIpPort(), "This is a test, please ignore");
+
+            assertTrue(report.isValid());
+
+            pocketCore.send(report, message -> {
+                assertNotNull(message);
+                assertThat(message, containsString("Okay"));
+                return Unit.INSTANCE;
+            });
+
+            return Unit.INSTANCE;
+        });
     }
 }
