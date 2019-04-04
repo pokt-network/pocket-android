52c984a28115b24772b376ee76e468f28616f061
Luis de Leon <luyzdeleon@gmail.com>
2019-04-03 21:49:44 -0400

    Added https support

--- pocket-android-core/src/main/java/network/pokt/core/model/Node.kt
+++ pocket-android-core/src/main/java/network/pokt/core/model/Node.kt
@@ -4,16 +4,38 @@ class Node(network: String, netId: String, ipPort: String) {
 
     var network = network
     var netId = netId
-    lateinit var ip: String
+    var ip: String
     var port: Int? = null
     var ipPort = ipPort
 
     private val nonSSLProtocol = "http://"
     private val SSLProtocol = "https://"
+    private val defaultSSLPort = 443
+    private val defaultHTTPPort = 80
 
     init {
+        val ipPortData = this.ipPort.split(":")
+        if (ipPortData.size == 2) {
+            this.ip = ipPortData[0]
+            val portStr = ipPortData[1]
+            this.port = when(portStr) {
+                "" -> defaultHTTPPort
+                null -> defaultHTTPPort
+                else -> portStr.toInt()
+            }
+        } else {
+            // Default to localhost effectively making this node error out on contact
+            this.ip = "localhost"
+            this.port = defaultHTTPPort
+        }
+
         if (!this.ipPort.contains(nonSSLProtocol) || !this.ipPort.contains(SSLProtocol)) {
-            this.ipPort = "$nonSSLProtocol$ipPort"
+            if (this.port == defaultSSLPort) {
+                this.ipPort = "$SSLProtocol$ipPort"
+            } else {
+                this.ipPort = "$nonSSLProtocol$ipPort"
+            }
+
         }
     }
 
@@ -23,6 +45,15 @@ class Node(network: String, netId: String, ipPort: String) {
         this.ip = ip
         this.port = port
         this.ipPort = ipPort
+
+        if (!this.ipPort.contains(nonSSLProtocol) || !this.ipPort.contains(SSLProtocol)) {
+            if (this.port == defaultSSLPort) {
+                this.ipPort = "$SSLProtocol$ipPort"
+            } else {
+                this.ipPort = "$nonSSLProtocol$ipPort"
+            }
+
+        }
     }
 
     fun isEqual(netId: String, network: String): Boolean {
