79faa7025a611ac54387d019842232e40437f22c
eugeniomancebo <eugenioamancebo@gmail.com>
2019-05-06 08:55:52 -0400

    Adding KDocs for the errors module

--- pocket-android-core/src/main/java/network/pocket/core/errors/PocketError.kt
+++ pocket-android-core/src/main/java/network/pocket/core/errors/PocketError.kt
@@ -1,3 +1,9 @@
 package network.pocket.core.errors
 
+/**
+ * A custom @see Error to encapsulate uncommon behaviour using the Pocket API
+ *
+ *
+ * @param errorMsg the error message to display
+ */
 class PocketError(errorMsg: String) : Error(errorMsg)
--- pocket-android-core/src/main/java/network/pocket/core/errors/WalletPersistenceError.kt
+++ pocket-android-core/src/main/java/network/pocket/core/errors/WalletPersistenceError.kt
@@ -1,3 +1,9 @@
 package network.pocket.core.errors
 
+/**
+ * A custom @see Error to encapsulate issues while storing or retrieving wallet info
+ *
+ *
+ * @param errorMsg the error message to display
+ */
 class WalletPersistenceError(errorMsg: String) : Error(errorMsg)
