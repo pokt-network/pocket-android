8816c3e1c322f5b99281924d08f434564e0c232e
eugeniomancebo <eugenioamancebo@gmail.com>
2019-05-06 08:58:10 -0400

    Adding Kdoc for util module

--- pocket-android-core/src/main/java/network/pocket/core/util/Utils.kt
+++ pocket-android-core/src/main/java/network/pocket/core/util/Utils.kt
@@ -3,6 +3,13 @@ package network.pocket.core.util
 class Utils {
 
     companion object {
+        /**
+         * Method used to pre-check for null or empty values.
+         *
+         * @throws Exception if params are invalid.
+         * @property values the values to be pre-checked
+         * @return true if values are valid
+         */
         @Throws(Exception::class)
         fun areDirty(vararg values: String): Boolean {
             for (value in values) {
