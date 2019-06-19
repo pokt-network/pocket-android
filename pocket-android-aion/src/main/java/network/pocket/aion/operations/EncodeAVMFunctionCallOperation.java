d317c44ed0e1554fee5a4dfca5814f2f1b296683
andrewnguyen22 <amnguyen1@mail.usf.edu>
2019-06-19 07:28:58 -0400

    encode/decode wip

--- pocket-android-aion/src/main/java/network/pocket/aion/operations/EncodeAVMFunctionCallOperation.java
+++ pocket-android-aion/src/main/java/network/pocket/aion/operations/EncodeAVMFunctionCallOperation.java
@@ -2,40 +2,16 @@ package network.pocket.aion.operations;
 
 import android.os.Build;
 import android.support.annotation.RequiresApi;
-import network.pocket.aion.aionSource.ABIEncoder;
+import network.pocket.aion.aionSource.ABIStreamingEncoder;
 
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.lang.reflect.Parameter;
 import java.util.ArrayList;
 
 import static network.pocket.aion.util.HexStringUtil.bytesToHex;
 
+// QUESTIONS: once I parse the function call and the parameter types (string), what do I do?
+// QUESTIONS: Then do I call ETH.SendTransaction or ETH.Call?
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class EncodeAVMFunctionCallOperation {
-    enum AVMTypes {
-        BYTE("byte"), BYTE1D("byte[]"), BYTE2D("byte[][]"),
-        BOOLEAN("bool"), BOOLEAN1D("bool[]"), BOOLEAN2D("bool[][]"),
-        CHAR("char"), CHAR1D("char[]"), CHAR2D("char[][]"),
-        SHORT("short"), SHORT1D("short[]"), SHORT2D("short[][]"),
-        INT("int"), INT1D("int[]"), INT2D("int[][]"),
-        FLOAT("float"), FLOAT1D("float[]"), FLOAT2D("float[][]"),
-        LONG("long"), LONG1D("long[]"), LONG2D("long[][]"),
-        DOUBLE("double"), DOUBLE1D("double[]"), DOUBLE2D("double[][]"),
-        ADDRESS("address"), ADDRESS1D("address[]"), STRING("string"),
-        String1D("string[]");
-
-        private final String name;
-
-        AVMTypes(final String name) {
-            this.name = name;
-        }
-
-        @Override
-        public String toString() {
-            return name;
-        }
-    }
-
     class Function {
         public String name;
         public String modifier;
@@ -43,7 +19,12 @@ public class EncodeAVMFunctionCallOperation {
         public ArrayList<String> params; // array of param<type / name>
     }
 
-    public Function FunctionParser(String rawFunction) { //TODO error handling
+    public String encodeFunction(String rawFunction) {
+        Function f = functionParser(rawFunction);
+        return encode(f);
+    }
+
+    private Function functionParser(String rawFunction) { //TODO error handling
         Function f = new Function();
         // split by `(` character
         String[] result = rawFunction.split("\\(");
@@ -69,32 +50,133 @@ public class EncodeAVMFunctionCallOperation {
         return f;
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public Function FunctionParserReflection(Method method) {
-        Function f = new Function();
-        f.modifier = Modifier.toString(method.getModifiers());
-        f.name = method.getName();
-        f.returnType = method.getReturnType().getSimpleName();
-        Parameter[] parameters = method.getParameters();
-        for (Parameter parameter : parameters) {
-            if (!parameter.isNamePresent()) {
-                throw new IllegalArgumentException("Parameter names are not present!");
-            }
-            f.params.add(parameter.getType().getSimpleName());
-        }
-        return f;
-    }
-
-    public void Encode(Function f) {
+    private String encode(Function f) {
+        ABIStreamingEncoder abiStreamingEncoder = new ABIStreamingEncoder();
         String result = "0x";
-        String name = bytesToHex(ABIEncoder.encodeOneString(f.name));
+        String name = bytesToHex(abiStreamingEncoder.encodeOneString(f.name).toBytes());
         result = result.concat(name);
-        for (String p : f.params) {
-
-        }
+        for (String p : f.params) result = result.concat(bytesToHex(abiStreamingEncoder.encodeOneString(p).toBytes()));
+        return result;
     }
 
-    private String ParameterEncode(String p){
-        // TODO
+    final String STRING1D = "String[]";
+    final String STRING = "String";
+    final String ADDRESS1D = "Address[]";
+    final String ADDRESS = "Address";
+    final String DOUBLE2D = "double[][]";
+    final String DOUBLE1D = "double[]";
+    final String DOUBLE = "double";
+    final String LONG2D = "long[][]";
+    final String LONG1D = "long[]";
+    final String LONG = "long";
+    final String FLOAT2D = "float[][]";
+    final String FLOAT1D = "float[]";
+    final String FLOAT = "float";
+    final String INT2D = "int[][]";
+    final String INT1D = "int[]";
+    final String INT = "int";
+    final String SHORT2D = "short[][]";
+    final String SHORT1D = "short[]";
+    final String SHORT = "short";
+    final String CHAR2D = "char[][]";
+    final String CHAR1D = "char[]";
+    final String CHAR = "char";
+    final String BOOLEAN2D = "bool[][]";
+    final String BOOLEAN1D = "bool[]";
+    final String BOOLEAN = "bool";
+    final String BYTE2D = "byte[][]";
+    final String BYTE1D = "byte[]";
+    final String BYTE = "byte";
+
+    private String encodeParam(String type){
+        ABIStreamingEncoder encoder = new ABIStreamingEncoder();
+        switch (type) {
+            case ADDRESS:
+                encoder.encodeOneAddress();
+                break;
+            case ADDRESS1D:
+                encoder.encodeOneAddressArray();
+                break;
+            case BYTE:
+                encoder.encodeOneByte();
+                break;
+            case BYTE1D:
+                encoder.encodeOneByteArray();
+                break;
+            case BYTE2D:
+                encoder.encodeOne2DByteArray();
+                break;
+            case BOOLEAN:
+                encoder.encodeOneBoolean();
+                break;
+            case BOOLEAN1D:
+                encoder.encodeOneBooleanArray();
+                break;
+            case BOOLEAN2D:
+                encoder.encodeOne2DBooleanArray();
+                break;
+            case CHAR:
+                encoder.encodeOneCharacter();
+                break;
+            case CHAR1D:
+                encoder.encodeOneCharacterArray();
+                break;
+            case CHAR2D:
+                encoder.encodeOne2DCharacterArray();
+                break;
+            case DOUBLE:
+                encoder.encodeOneDouble();
+                break;
+            case DOUBLE1D:
+                encoder.encodeOneDoubleArray();
+                break;
+            case DOUBLE2D:
+                encoder.encodeOne2DDoubleArray();
+                break;
+            case FLOAT:
+                encoder.encodeOneFloat();
+                break;
+            case FLOAT1D:
+                encoder.encodeOneFloatArray();
+                break;
+            case FLOAT2D:
+                encoder.encodeOne2DFloatArray();
+                break;
+            case INT:
+                encoder.encodeOneInteger();
+                break;
+            case INT1D:
+                encoder.encodeOneIntegerArray();
+                break;
+            case INT2D:
+                encoder.encodeOne2DIntegerArray();
+                break;
+            case LONG:
+                encoder.encodeOneLong();
+                break;
+            case LONG1D:
+                encoder.encodeOneLongArray();
+                break;
+            case LONG2D:
+                encoder.encodeOne2DLongArray();
+                break;
+            case SHORT:
+                encoder.encodeOneShort();
+                break;
+            case SHORT1D:
+                encoder.encodeOneShortArray();
+                break;
+            case SHORT2D:
+                encoder.encodeOne2DShortArray();
+                break;
+            case STRING:
+                encoder.encodeOneString();
+                break;
+            case STRING1D:
+                encoder.encodeOneStringArray();
+                break;
+            default:
+                break;
+        }
     }
 }
