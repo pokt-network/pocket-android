8cc03d2cb05c475c154a27a30eb12beb67b72c13
Andrew Nguyen <amnguyen1@mail.usf.edu>
2019-06-26 13:05:55 -0400

    added aion AVM support

--- pocket-android-aion/src/main/java/network/pocket/aion/util/HexStringUtil.java
+++ pocket-android-aion/src/main/java/network/pocket/aion/util/HexStringUtil.java
@@ -2,7 +2,6 @@ package network.pocket.aion.util;
 
 /**
  * Utility class for Hex parsing.
- *
  */
 public class HexStringUtil {
 
@@ -32,4 +31,25 @@ public class HexStringUtil {
         return hex.length() >= ZERO_X.length() && hex.substring(0, 2).equalsIgnoreCase(ZERO_X);
     }
 
+    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    public static byte[] hexToBytes(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
+    }
 }
