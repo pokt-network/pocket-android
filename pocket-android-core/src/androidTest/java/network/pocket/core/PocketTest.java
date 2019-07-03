769c6a5dbfb751e0afda04a32e4a8a76568c2745
Andrew Nguyen <amnguyen1@mail.usf.edu>
2019-07-03 13:53:42 -0400

    added REST functionality

--- pocket-android-core/src/androidTest/java/network/pocket/core/PocketTest.java
+++ pocket-android-core/src/androidTest/java/network/pocket/core/PocketTest.java
@@ -1,7 +1,10 @@
 package network.pocket.core;
 
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
+import android.util.Pair;
 import kotlin.Unit;
+import network.pocket.core.model.NewRelay;
 import network.pocket.core.model.Relay;
 import network.pocket.core.util.PocketTestPlugin;
 import network.pocket.core.util.SemaphoreUtil;
@@ -12,6 +15,8 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Semaphore;
 
 import static org.junit.Assert.*;
@@ -32,7 +37,7 @@ public class PocketTest {
 
     @Before
     public void setup() {
-        this.plugin = new PocketTestPlugin("DEVID1", "ETH", new String[]{"4"}, 5, 60000);
+        this.plugin = new PocketTestPlugin("DEVO7QQqPHCK2h3cGXhh2rY", "ETH", new String[]{"4"}, 5, 60000);
         assertNotNull(this.plugin);
     }
 
@@ -44,13 +49,13 @@ public class PocketTest {
     @Test
     public void testNoNodesAvailable() {
         // Note the invalid networkName on the constructor
-        PocketTestPlugin plugin = new PocketTestPlugin("DEVID1", "TEST", new String[]{"4"}, 5, 60000);
+        PocketTestPlugin plugin = new PocketTestPlugin("DEVO7QQqPHCK2h3cGXhh2rY", "TEST", new String[]{"4"}, 5, 60000);
         SemaphoreUtil.executeSemaphoreCallback(new SemaphoreUtil.SemaphoreCallback() {
             @Override
             public void execute(Semaphore semaphore) {
                 String address = "0xf892400Dc3C5a5eeBc96070ccd575D6A720F0F9f";
                 String data = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBalance\",\"params\":[\"".concat(address).concat("\",\"latest\"],\"id\":67}");
-                Relay relay = new Relay("TEST", "4", "DEVID1", data);
+                NewRelay relay = new NewRelay("ETH", "4", "DEVO7QQqPHCK2h3cGXhh2rY", data, "", "", new ArrayList<>());
                 plugin.send(relay, (pocketError, jsonObject) -> {
                     assertNotNull(pocketError);
                     assertNull(jsonObject);
@@ -70,7 +75,7 @@ public class PocketTest {
             public void execute(Semaphore semaphore) {
                 String address = "0xf892400Dc3C5a5eeBc96070ccd575D6A720F0F9f";
                 String data = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBalance\",\"params\":[\"".concat(address).concat("\",\"latest\"],\"id\":67}");
-                Relay relay = new Relay("ETH", "4", "DEVID1", data);
+                NewRelay relay = new NewRelay("ETH", "4", "DEVO7QQqPHCK2h3cGXhh2rY", data, "", "", new ArrayList<>());
                 plugin.send(relay, (pocketError, jsonObject) -> {
                     assertNull(pocketError);
                     assertNotNull(jsonObject);
@@ -91,7 +96,7 @@ public class PocketTest {
             public void execute(Semaphore semaphore) {
                 String address = "0xf892400Dc3C5a5eeBc96070ccd575D6A720F0F9f";
                 String data = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBalance\",\"params\":[\"".concat(address).concat("\",\"latest\"],\"id\":67}");
-                plugin.send("ETH", "4", data, (pocketError, jsonObject) -> {
+                plugin.send("ETH", "4", data, "", "", new ArrayList<>(), (pocketError, jsonObject) -> {
                      assertNull(pocketError);
                     assertNotNull(jsonObject);
                     semaphore.release();
